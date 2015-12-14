6fdead3f69a7c8d4c0ecbdf17e2a326d13b89b60
W <dqwcn@foxmail.com>
2015-12-14 14:01:46 +0800

    Initial Commit

---
+++ GroupText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GroupText
+//
+//  Created by W on 15/12/14.
+//  Copyright © 2015年 IEC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GroupText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GroupText
+//
+//  Created by W on 15/12/14.
+//  Copyright © 2015年 IEC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
